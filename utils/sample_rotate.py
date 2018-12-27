6e6146f0b780e640fe6f7a601a3fcc59428fe104
kenanhai <2579007133@qq.com>
2018-12-27 20:30:36 +0800

    Update sample_rotate.py

--- utils/sample_rotate.py
+++ utils/sample_rotate.py
@@ -1,7 +1,14 @@
+import os, sys
 import cv2
-import os
-import numpy as np
-import glob
+import tqdm
+
+rate = 3
+
+date = "nir_real"
+src = os.path.join("/mnt/disk0/kenanhai/NIR/nir", date)
+dst = os.path.join("/mnt/disk0/kenanhai/NIR/nir/nir_real_split", date)
+if not os.path.exists(dst):
+    os.makedirs(dst)
 
 def rotate_image(mat, angle):
   # angle in degrees
@@ -23,26 +30,32 @@ def rotate_image(mat, angle):
   rotated_mat = cv2.warpAffine(mat, rotation_mat, (bound_w, bound_h))
   return rotated_mat
 
-def mkdir_path(path):
-    if not os.path.exists(path):
-        os.makedirs(path)
 
-def main():
-    root_path = './imgs-1029'
-    img_path=root_path+'/oppo-a57'
-    save_path=root_path+'/imgs-crop-sun-1029'
-    mkdir_path(save_path)
-
-    img_list = glob.glob(img_path+'/*.png')
-    print((img_list[0]))
-
-    i=0
-    for item in img_list:
-        i+=1
-        print("%d/%d path:%s"%(i,len(img_list), item))
-        name  =  os.path.splitext(os.path.basename(item))[0]
-        img = cv2.imread(item)
-        img_rotation = rotate_image(img, 90)
-        cv2.imwrite(save_path+'/'+name+'.jpg', img_rotation)
-if __name__ == '__main__':
-    main()
+videos = sorted(os.listdir(src))
+for video in videos:
+    print (video)
+    cap = cv2.VideoCapture(os.path.join(src, video))
+    if not cap.isOpened():
+        print ("Error:", video)
+        continue
+    frames = int(cap.get(cv2.CAP_PROP_FRAME_COUNT))
+    width = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
+    height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
+    fps = int(cap.get(cv2.CAP_PROP_FPS))
+    print (frames, width, height, fps)
+    pbar = tqdm.tqdm(total=len(range(0, frames, rate)))
+    for frame in range(0, frames, rate):
+        cap.set(cv2.CAP_PROP_POS_FRAMES, frame)
+        ret, mat = cap.read()
+        #mat=cv2.resize(mat,(1280,720),interpolation=cv2.INTER_CUBIC)
+        if not ret:
+            print ("Error:", video, frame)
+            continue
+        img = os.path.splitext(os.path.basename(video))[0]
+        img += "_{}.jpg".format(frame)
+        #mat = rotate_image(mat, -90)
+        cv2.imwrite(os.path.join(dst, img), mat)
+        pbar.update(1)
+    pbar.close()
+    cap.release()
+    print (video, "done")
